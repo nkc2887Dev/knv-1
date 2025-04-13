4915e93bc45c85918d7aa90a5a99f38f4fade577
nkc2887dev <nkc2887dev@gmail.com>
2025-04-14 00:01:00 +0530

    movies

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/knv-1/",
+  base: "/knv-1",
   plugins: [react()],
   build: {
     outDir: 'dist',  // Ensure this points to the right directory for output files
