48beeec47a99f53a7db872f58c2c0fb6cb0d41e5
nkc2887dev <nkc2887dev@gmail.com>
2025-04-13 22:59:38 +0530

    movies

--- src/config/processEnv.ts
+++ src/config/processEnv.ts
@@ -1,5 +1,5 @@
 export const config = {
-    API_KEY: import.meta.env.VITE_API_KEY,
-    API_URL: import.meta.env.VITE_API_URL,
-    IMDB_ID: import.meta.env.VITE_IMDB_ID,
-}
+  API_KEY: import.meta.env.VITE_API_KEY || "3d4de608",
+  API_URL: import.meta.env.VITE_API_URL || "http://www.omdbapi.com/",
+  IMDB_ID: import.meta.env.VITE_IMDB_ID || "tt3896198",
+};
